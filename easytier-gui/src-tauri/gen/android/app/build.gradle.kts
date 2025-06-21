533e950e94bff4c8d976594cb21aed8b440067d1
avkiller <avkiller2012@gmail.com>
2025-06-21 12:13:05 +0800

    Update build.gradle.kts
    修改信息

--- easytier-gui/src-tauri/gen/android/app/build.gradle.kts
+++ easytier-gui/src-tauri/gen/android/app/build.gradle.kts
@@ -19,25 +19,11 @@ android {
     namespace = "com.kkrainbow.easytier"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
-        applicationId = "com.kkrainbow.easytier"
+        applicationId = "com.fireworld.easytier"
         minSdk = 24
         targetSdk = 34
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
-    }
-    signingConfigs {
-        create("release") {
-            val keystorePropertiesFile = rootProject.file("keystore.properties")
-            val keystoreProperties = Properties()
-            if (keystorePropertiesFile.exists()) {
-                keystoreProperties.load(FileInputStream(keystorePropertiesFile))
-            }
-
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
-        }
     }
     buildTypes {
         getByName("debug") {

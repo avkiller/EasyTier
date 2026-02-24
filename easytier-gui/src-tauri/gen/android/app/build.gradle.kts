ab83e485ac9fdcad868936712ae1026cda018149
avkiller <avkiller2012@gmail.com>
2026-02-24 08:47:38 +0800

    Update build.gradle.kts

--- easytier-gui/src-tauri/gen/android/app/build.gradle.kts
+++ easytier-gui/src-tauri/gen/android/app/build.gradle.kts
@@ -24,9 +24,6 @@ android {
         targetSdk = 34
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
-        ndk {
-          abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a"))
-        }
     }
     signingConfigs {
         create("release") {
